064b24f4e8113acfa30dcdf30bbecca9444e3365
tehmou <timo.tuominen@gmail.com>
2017-02-21 11:44:54 -0300

    Show dialog with the title only

--- app/src/main/java/com/tehmou/book/androidwithlatestfromexercise/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidwithlatestfromexercise/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tehmou.book.androidwithlatestfromexercise;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -8,6 +9,7 @@ import com.jakewharton.rxbinding2.view.RxView;
 import com.jakewharton.rxbinding2.widget.RxTextView;
 
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,15 +30,18 @@ public class MainActivity extends AppCompatActivity {
 
         final Observable<Object> clickEvents =
                 RxView.clicks(findViewById(R.id.action_button));
+        
+        // Convert the Void clicks into the necessary information to show the dialog
+        final Observable<String> showDialogEventObservable =
+                clickEvents.withLatestFrom(titleObservable, (ignore, title) -> title);
 
-
-        /**
-         * Show a dialog based on the Observables
-         *
-         * new AlertDialog.Builder(this)
-         *   .setTitle(...)
-         *   .setMessage(...)
-         *   .show();
-        **/
+        // Subscribe to the event Observable that sends us the necessary information to show the
+        // dialog at the time when it should be shown.
+        showDialogEventObservable
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(title ->
+                        new AlertDialog.Builder(this)
+                                .setTitle(title)
+                                .show());
     }
 }
